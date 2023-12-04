6659c433a6d71776d135b3512f2056e92ab3d97c
Federico Veron <veronfederico45@gmail.com>
2023-12-04 20:13:41 -0300

    cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const serialRoutes = require('./src/routes/serialRoutes');
+const cors = require('cors'); // Importa el paquete cors
 
 const app = express();
 const port = 3000;
@@ -9,6 +10,8 @@ const uri = "mongodb+srv://fedeveron2:SGk3iFXPrJkrk5Hi@sysgestion.jv83i.mongodb.
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(express.json());
+app.use(cors());
+
 app.use('/serial', serialRoutes);
 
 app.listen(port, () => {
